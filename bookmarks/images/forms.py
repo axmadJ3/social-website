829b4f7a56fdf5cf89ff5cf62d8f99244f2190d2
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-04-23 21:44:59 +0500

    created image view adn bookmarklet

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,4 +1,8 @@
+import requests
+
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 from .models import Image
 
@@ -21,3 +25,19 @@ class ImageCreateForm(forms.ModelForm):
             )
         return url
     
+    def save(self, forse_insert=False, forse_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        response = requests.get(image_url)
+        image.image.save(
+            image_name,
+            ContentFile(response.content),
+            save=False
+        )
+        if commit:
+            image.save()
+        return image
+        
